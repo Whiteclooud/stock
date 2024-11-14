5076efc25795c6afb7931925934e605376ecd296
兰天 <10113170+whiteclooud@user.noreply.gitee.com>
2024-11-14 18:29:43 +0800

    项目集成Swagger

---
+++ stock_backend/src/main/java/com/lan/stock/config/SwaggerConfiguration.java
@@ -0,0 +1,45 @@
+package com.lan.stock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author lan
+ * @version 1.0
+ * @description: 定义swagger配置类
+ * @date 2024/11/14 11:39
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket buildDocket() {
+        //构建在线API概要对象
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInfo())
+                .select()
+                // 要扫描的API(Controller)基础包
+                .apis(RequestHandlerSelectors.basePackage("com.lan.stock.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo buildApiInfo() {
+        //网站联系方式
+        Contact contact = new Contact("黑马程序员","https://www.itheima.com/","itcast@163.com");
+        return new ApiInfoBuilder()
+                .title("今日指数-在线接口API文档")//文档标题
+                .description("这是一个方便前后端开发人员快速了解开发接口需求的在线接口API文档")//文档描述信息
+                .contact(contact)//站点联系人相关信息
+                .version("1.0.0")//文档版本
+                .build();
+    }
+}
